68dd972a4b594e876127937b9ab724cf362f09f4
fabiopozzo <93141794+fabiopozzo@users.noreply.github.com>
2022-02-11 19:17:57 -0600

    Update FastMath.cpp

--- lib/FastMath.cpp
+++ lib/FastMath.cpp
@@ -43,7 +43,7 @@ FastMath::ArcTangents::ArcTangents()
         static Values v;
         for (auto x = Min; x < Max; ++x) {
             for (auto y = Min; y < Max; ++y) {
-                const auto t = ::atan2f(x, y);
+                auto t = ::atan2f(x, y);
                 if (t < 0) {
                     t += TwoPI;
                 }
